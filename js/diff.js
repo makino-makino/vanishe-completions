5b288a05cc14d6aac5d2bcbb7fda0d37e3459ad5
akakou <akakou571@gmail.com>
2020-03-22 19:37:05 +0900

    差分を出す関数を作成

--- js/diff.js
+++ js/diff.js
@@ -1,20 +1,74 @@
+// オーダーなんてしらない
+const getLastSameCharIndex = (longer, shorter) => {
+  var result = null;
+
+  shorter.forEach((value, index, list) => {
+    if (result) return
+    
+    const l = longer[index];
+    const s = shorter[index];
+
+    console.log(l, s)
+    console.log(l != s)
+
+    if (l != s) {
+      result = index;
+      return;
+    }
+  });
+
+  return result
+};
+
+const calcDiff = (before, after) => {
+  var longer = null;
+  var shorter = null;
+
+  before = before.split('')
+  after = after.split('')
+
+  if (before.length > after.length) {
+    longer = before;
+    shorter = after;
+  } else {
+    longer = after;
+    shorter = before;
+  }
+
+  const first = getLastSameCharIndex(longer=longer, shorter=shorter);
+
+  longer.reverse()
+  shorter.reverse()
+
+  const last = getLastSameCharIndex(longer=longer, shorter=shorter)
+
+  longer.reverse()
+  const diff = after.slice(first, after.length - last).join('')
+
+  return {diff, first, last}
+};
+
 class currentDiff {
   constructor(io) {
-    this.io = io
-    this.before = ''
+    this.io = io;
+    this.before = "";
   }
 
   read() {
-    const msg = this.io.read()
-    this.before = msg
+    const msg = this.io.read();
+    this.before = msg;
 
     // ここで差分を探す
-    diff = '';
+    diff = "";
     return diff;
   }
 
   write(msg) {
-    this.io.write(msg)
-    this.reset()
+    this.io.write(msg);
+    this.reset();
   }
 }
+
+a = calcDiff("私、赤間です", "私は構成です");
+
+console.log(a)
