8878a648fe7acbb7885da800981d4a0df748e6cc
akakou <akakou571@gmail.com>
2020-03-22 17:34:52 +0900

    抽象化した

--- js/content.js
+++ js/content.js
@@ -1,15 +1,39 @@
+const TYPES = {
+  INNER_TEXT: 'innerText',
+  VALUE: 'value'
+}
+
+class ElementIO {
+  constructor(element, type) {
+    this.element = element;
+    this.type = type;
+  }
+  
+  read(msg) {
+      console.log(this.element, this.type)
+      return this.element[this.type];
+  }
+
+  write(msg) {
+    this.element[this.type] = msg;
+  } 
+}
+
 window.addEventListener('input', e => {
-  var inputValue = ''
+  const target = e.target
 
-  if (e.target.value) { 
-    inputValue = e.target.value
-    console.log('a')
-  }
+  var type = null
 
-  else if(e.target.innerText) {
-    inputValue = e.target.innerText
-    console.log('b')
-  }
+  if (target.value) 
+    type = TYPES.VALUE;
 
-  console.log(inputValue);
+  else if(target.innerText)
+    type = TYPES.INNER_TEXT;
+
+  else return;
+
+  
+  const io = new ElementIO(element=target, type=type);
+  console.log(io.read());
+
 }, false);
