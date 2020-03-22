72bc58e096de14b3848536ef9a5906d9e384675d
akakou <akakou571@gmail.com>
2020-03-23 01:37:59 +0900

    F2が押されたときに、差分を出すようにした

--- js/content.js
+++ js/content.js
@@ -1,3 +1,8 @@
+const diffTaker = new DiffTaker();
+const F2_KEYCODE = 113;
+
+var userInput = "";
+
 window.addEventListener(
   "input",
   e => {
@@ -9,8 +14,19 @@ window.addEventListener(
     else if (target.innerText) type = TYPES.INNER_TEXT;
     else return;
 
-    const io = new ElementIO((element = target), (type = type));
-    console.log(io.read());
+    const io = new ElementIO(target, type);
+    userInput = io.read();
+  },
+  false
+);
+
+window.addEventListener(
+  "keydown",
+  e => {
+    if (e.keyCode == F2_KEYCODE) {
+      const result = diffTaker.diff(userInput);
+      diffTaker.commit(userInput);
+    }
   },
   false
 );
