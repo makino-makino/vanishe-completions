e2052734df59b0a029ab428dc52113c79cdfdff2
akakou <akakou571@gmail.com>
2020-03-23 00:55:46 +0900

    テストコードの追加

---
+++ tests/diff.js
@@ -0,0 +1,60 @@
+/**
+ * @todo ちゃんとしたunit testツールを利用する
+ */
+
+const { DiffTaker, takeDiff, getLastSameCharIndex } = require("../js/diff.js");
+
+const testGetLastSameCharIndex1 = () => {
+  const shorter = [1, 2, 3, 4, 5, 6];
+  const longer = [1, 2, 3, 4, 4, 3, 2];
+
+  const expected = 4;
+  const result = getLastSameCharIndex({ longer, shorter });
+
+  console.log(expected == result);
+};
+
+const testGetLastSameCharIndex2 = () => {
+  const shorter = [2, 2, 3, 2, 1, 1];
+  const longer = [1, 1, 2, 4, 4, 3, 2];
+
+  const expected = 0;
+  const result = getLastSameCharIndex({ longer, shorter });
+
+  console.log(expected == result);
+};
+
+const testTakeDiff = () => {
+  const before = "ばにしぇだぞ〜〜〜ｗｗｗｗｗ";
+  const after = "ばにしぇってなんやねん";
+
+  const expected = "ってなんやねん";
+  const result = takeDiff({ before, after });
+
+  console.log(result.diff == expected);
+};
+
+const testDiffTaker = () => {
+  const diffTaker = new DiffTaker();
+
+  const before = "ばにしぇだぞ〜〜〜ｗｗｗｗｗ";
+  const after = "ばにしぇってなんやねん";
+
+  const expected = "ってなんやねん";
+
+  diffTaker.commit(before);
+  const result = diffTaker.diff(after);
+
+  console.log(result.diff == expected);
+};
+
+console.log("getLastSameCharIndex:");
+testGetLastSameCharIndex1();
+
+testGetLastSameCharIndex2();
+
+console.log("takeDiff:");
+testTakeDiff();
+
+console.log("DiffTaker:");
+testDiffTaker();
