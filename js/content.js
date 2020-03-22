c063a7b6b064bc340101f1625f56df903fd1c3e7
akakou <akakou571@gmail.com>
2020-03-22 17:52:27 +0900

    ファイルわけした

--- js/content.js
+++ js/content.js
@@ -1,24 +1,3 @@
-const TYPES = {
-  INNER_TEXT: 'innerText',
-  VALUE: 'value'
-}
-
-class ElementIO {
-  constructor(element, type) {
-    this.element = element;
-    this.type = type;
-  }
-  
-  read(msg) {
-      console.log(this.element, this.type)
-      return this.element[this.type];
-  }
-
-  write(msg) {
-    this.element[this.type] = msg;
-  } 
-}
-
 window.addEventListener('input', e => {
   const target = e.target
 
---
+++ js/io.js
@@ -0,0 +1,20 @@
+const TYPES = {
+  INNER_TEXT: 'innerText',
+  VALUE: 'value'
+}
+
+class ElementIO {
+  constructor(element, type) {
+    this.element = element;
+    this.type = type;
+  }
+  
+  read(msg) {
+      console.log(this.element, this.type)
+      return this.element[this.type];
+  }
+
+  write(msg) {
+    this.element[this.type] = msg;
+  } 
+}
