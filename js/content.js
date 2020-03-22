92ed9a9af5283f0976bf0df3bfb08cd4ce6c0f16
akakou <akakou571@gmail.com>
2020-03-22 16:41:24 +0900

    innerHTMLはとりきれてなかったのでとった

--- js/content.js
+++ js/content.js
@@ -1,3 +1,15 @@
 window.addEventListener('input', e => {
-  console.log(e.target.value);
+  var inputValue = ''
+
+  if (e.target.value) { 
+    inputValue = e.target.value
+    console.log('a')
+  }
+
+  else if(e.target.innerText) {
+    inputValue = e.target.innerText
+    console.log('b')
+  }
+
+  console.log(inputValue);
 }, false);
