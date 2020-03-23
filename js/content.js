761a487d370277c631fa0a6c87fab9090551b5eb
akakou <akakou571@gmail.com>
2020-03-23 16:58:06 +0900

    変換が実際にできるようにした

--- js/content.js
+++ js/content.js
@@ -5,33 +5,46 @@ var userInput = "";
 var io = null;
 const henkan = new Henkan();
 
-window.addEventListener(
-  "input",
-  e => {
-    const target = e.target;
+const reloadUserInput = () => {
+  var type = null;
 
-    var type = null;
+  const target = document.activeElement;
 
-    if (target.value) type = TYPES.VALUE;
-    else if (target.innerText) type = TYPES.INNER_TEXT;
-    else return;
+  if (target.value) type = TYPES.VALUE;
+  else if (target.innerText) type = TYPES.INNER_TEXT;
+  else return;
 
-    io = new ElementIO(target, type);
-    userInput = io.read();
-  },
-  false
-);
+  io = new ElementIO(target, type);
+  userInput = io.read();
+  return userInput;
+};
+
+window.addEventListener("keypress", e => reloadUserInput, false);
 
 window.addEventListener(
   "keydown",
   e => {
     if (e.keyCode == F2_KEYCODE) {
-      const { diff, first, last } = diffTaker.diff(userInput);
-      diffTaker.commit(userInput);
+      reloadUserInput();
 
-      const result = henkan.henkan(diff, 1);
+      let { diff, first, last } = diffTaker.diff(userInput);
 
-      console.log(result);
+      // diffTaker.commit(userInput);
+
+      // console.log({ diff, first, last });
+
+      const henkaned_word = henkan.henkan(diff, 1);
+
+      // console.log(henkaned_word);
+
+      const result = diffTaker.apply({
+        base: userInput,
+        patch: henkaned_word,
+        first,
+        last
+      });
+
+      io.write(result);
     }
   },
   false
